3ef1b8ec8a1e6f9ab236c1e565101448bd5bc547
rcarleton01 <rcarleton01@qub.ac.uk>
2017-02-06 21:56:52 +0000

    available booking alert

--- dissertation/appointment_frm.php
+++ dissertation/appointment_frm.php
@@ -1,21 +1,38 @@
 <?php
 
 // Database Connection
-include('connect.php');
+include('session.php');
 
 // Form Variables
 $date = $_POST['date'];
 $time = $_POST['time'];
 
-// SQL Query
-$sql = "INSERT INTO appointment (appointment_date, appointment_time) VALUES ('$date', '$time')";
+$findbooking = "select * from appointment where appointment_date='$date' and appointment_time='$time'";
+if ($result=mysqli_query($mysqli,$findbooking))
+{
+// Return the number of rows in result set
+    $rowcount=mysqli_num_rows($result);
+    if ($rowcount > 0) { 
+       echo '<script type="text/javascript">
+       alert("Sorry, there is no appointment available.");
+       window.location.href = "appointment.php";
+       </script>';
+       exit; 
+    } else {
+        
+        $sql = "INSERT INTO appointment (appointment_date, appointment_time) VALUES ('$date', '$time')";
 
-if (mysqli_query($db, $sql)) {
-    echo "New appointment has been created successfully" . "<br /> - Appointment at " . $time . " on " . $date; 
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+        if (mysqli_query($mysqli, $sql)) {
+            echo "New appointment has been created successfully" . "<br /> - Appointment at " . $time . " on " . $date; 
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($db);
+        }
+        
+    }
+// Free result set
+    mysqli_free_result($result);
 }
 
-mysqli_close($db);
+mysqli_close($mysqli);
 
 ?>
